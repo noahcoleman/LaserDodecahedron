4662d9da2d2ebbda1edd591dca1e1bb76619807c
noahcoleman <noah.s.coleman@gmail.com>
2016-04-21 16:04:29 -0500

    edited comments, added mulitple off commands when gallery closes

--- timeSender.py
+++ timeSender.py
@@ -12,7 +12,7 @@ from datetime import time
 # total travel on y = 400
 
 # GET FILES FROM CORRECT DIRECTORY
-os.chdir('/Users/Noah/Documents/Python/ncFiles') # change present working directory to one with all the files
+os.chdir('/Users/Noah/Documents/Python/LaserDodecahedron/ncFiles') # change present working directory to one with all the files
 allFiles = glob.glob('*.nc')
 # print allFiles
 
@@ -29,15 +29,15 @@ def initializeMachine(s):
 def beginDraw(s):
     print "begin drawing"
     # If we add this we "force" the machine to always be in an on state, which might not be true if it was in an "off" state when it turned off.
-    # printAndWait(s, "M3S255\n") # turn on laser
+    printAndWait(s, "M3S255\n") # turn on laser
 
 def endDraw(s):
+    print "endDraw"
     printAndWait(s, "M5\n") # turn off laser
-    print "endDraw M5"
     printAndWait(s, "G0X0Y0\n") # go to 0,0
 
 
-def getNextLine():
+def getNextLine(): # reads all the lines from a file into an array and gets the next available line to send
     global files
     global lines
 
@@ -52,37 +52,35 @@ def getNextLine():
         files = allFiles[:]
         return getNextLine()
 
-def printAndWait(s, message): # send a line to serial and wait for a carriage return
+def printAndWait(s, message): # send a line to serial and wait for "ok" message from grbl
     print message
     if(s.port is None):
-        # Pretend like we're waiting ...
-        # clock.sleep(0.5)
         print "Not connected."
     else:
         s.write(message)
         code = s.readline()
-        print code #print to console for debugging
+        # print code #print to console for debugging
 
 def shouldBeOn():
     # compare times/ time comparisons compare time to range
     today = datetime.now()
-    testDay = 3 # dummy value for testing
+    # testDay = 3 # dummy value for testing
     weekday = datetime.weekday(today)
     if 1 < weekday < 5:
-        print "gallery is open today"
+        print "Gallery is open today",
         openTime = time(11, 00) # gallery opens at 11:00
         closeTime = time(18, 00) # gallery closes at 6:00
-        testTime = time(18, 30) # dummy value for testing
+        # testTime = time(18, 30) # dummy value for testing
         t = datetime.time(today)
         if closeTime > t > openTime:
-            print "gallery is open now"
-            return(1)
+            print "and gallery is open now."
+            return(True)
         else:
-            print "gallery is closed now"
-            return(0)
+            print "and gallery is closed now."
+            return(False)
     else:
-        print "gallery is closed today"
-        return (0)
+        print "Gallery is closed today."
+        return (False)
 
 def main():
     print("Let's get this laser party started!")
@@ -111,22 +109,24 @@ def main():
     wasOn = False
 
     # HERE IS WHERE ALL THE THINGS THAT SHOULD HAPPEN GO ONCE SERIAL CONNECTION IS ESTABLISHED.
-    while True: #if it is 1, then the program freezes
+    while True: 
         if shouldBeOn():
             if not wasOn:
                 beginDraw(s)
                 wasOn = True
-
             # Draw the next line.
             printAndWait(s, getNextLine())
 
         else:
             if wasOn:
-                endDraw(s)
-                print "M5 G0X0Y0"
+                i = 0
+                while i < 10:
+                    endDraw(s)
+                    clock.sleep(1)
+                    i = 1 + i
                 wasOn = False
-            # Sleep it for a bit so the while loop doesn't go out of control.
 
+        # Sleep it for a bit so the while loop doesn't go out of control.
         clock.sleep(0.1) # Comment this out when running real code--response from gShield will act as delay
 
 
